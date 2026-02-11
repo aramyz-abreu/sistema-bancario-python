9bf90e4f3a94b43950e4758ee4638af909f199bf
Aramyz <aramyzabreuquedi@gmail.com>
2026-02-11 07:39:10 -0400

    Corrigido o metodo de saque e deposito

--- conta_bancaria.py
+++ conta_bancaria.py
@@ -1,11 +1,12 @@
 class ContaBancaria:
-    limite_saque = 1
-    extrato = []    
 
-    def __init__(self, cliente, conta, saldo):
+    def __init__(self, cliente, conta, saldo, limite_saque):
         self.cliente = cliente
         self.conta = conta
         self.saldo = saldo
+        self.extrato = []
+        self.limite_saque = limite_saque
+
 
     def depositar(self, valor_deposito):
         self.valor_deposito = valor_deposito
@@ -15,34 +16,43 @@ class ContaBancaria:
         else:
             self.saldo += valor_deposito
             print(f"Depósito realizado com sucesso. Seu novo saldo é de R${self.saldo:.2f}")
-            ContaBancaria.extrato.append(f"Depósito: R${valor_deposito:.2f}. Novo Saldo: R${self.saldo:.2f}")
+            self.extrato.append(f"Depósito: R${valor_deposito:.2f}. Saldo após o DEPÓSITO: R${self.saldo:.2f}")
 
     def sacar(self, valor_saque):
         self.valor_saque = valor_saque
         
-        if ContaBancaria.limite_saque > 3:
-            print("Limite de saque excedido. Tente novamente outro dia")
+        if self.limite_saque >= 3:
+            print("Seu limite de saques diários foi excedido. Tente novamente outro dia")
 
         elif valor_saque > 500:
             print("Seu limite diário para saque é de R$ 500,00")
 
         elif self.saldo < valor_saque:
-            print(f"Saldo insuficiente! Saldo atual de {self.saldo:.2f}")
+            print(f"Saldo insuficiente! Saldo atual de R${self.saldo:.2f}")
             
         else:
             self.saldo -= valor_saque
-            print(f"Saque no valor de R${valor_saque:.2f} realizado com sucesso.\nSeu novo saldo é de {self.saldo:.2f}")
-            ContaBancaria.extrato.append(f"Saque: R${valor_saque:.2f}. Novo Saldo: R${self.saldo}")
-            ContaBancaria.limite_saque += 1
+            print(f"Saque no valor de R${valor_saque:.2f} realizado com sucesso.\nSeu novo saldo é de R${self.saldo:.2f}")
+            self.extrato.append(f"Saque: R${valor_saque:.2f}. Saldo após o SAQUE: R${self.saldo:.2f}")
+            self.limite_saque += 1
 
     def extrato_bancario(self):
         
-        return print(ContaBancaria.extrato)
+        
+        if not self.extrato:            
+            print("Não foram realizadas movimentações")
+        else:
+
+            print(self.extrato)
+            print(f"Seu saldo atual é de R${self.saldo:.2f}")
         
 
-cliente = ContaBancaria("Aramyz", 1, 600)
-cliente.depositar(30)
-cliente.sacar(350.35)
+cliente = ContaBancaria("Aramyz", 1, 0, 0)
+cliente.depositar(400)
+cliente.sacar(20)
+cliente.sacar(10)
+cliente.sacar(10)
+cliente.sacar(10)
 
-cliente.extrato_bancario()
+# cliente.extrato_bancario()
 
